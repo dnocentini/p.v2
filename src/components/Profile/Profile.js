7d2e20cd9d665690a0df62f71f8d3bcb2caac3e2
Dinny Nocentini <dinny.nocentini@gmail.com>
2021-07-09 12:50:39 -0500

    implement Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Typography } from '@material-ui/core'
+import React from 'react';
+
+import './Profile.css';
 
 const Profile = () => {
     return (
-        <div>
-            Profile Component
+        <div className="profile container_shadow">
+            <div className="profile_name">
+                <Typography className="name">Dinny Nocentini</Typography>
+                <Typography className="title">Software Engineer</Typography>
+            </div>
+            <figure className="profile_image"> 
+                <img src={require('../../assets/images/me.jpg').default} alt=""/>
+            </figure>
+            <div className="profile_information">
+                Timeline
+                <br/>
+                <button>Download Cv</button>
+
+            </div>
         </div>
     )
 }
