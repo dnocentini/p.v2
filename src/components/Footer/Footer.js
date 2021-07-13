49dcba370fe28c24450a5b14b7d9a328e4227b6a
Dinny Nocentini <dinny.nocentini@gmail.com>
2021-07-13 09:46:43 -0500

    implement Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import resumeData from '../../utils/resumeData';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            Footer Component
+        <div className='footer'>
+            <div className='footer_left'>
+                <Typography className='footer_name'>{resumeData.name}.</Typography>
+            </div>
+            <div className='footer_right'>
+                <Typography className='footer_copyright'>
+                    Developed and Designed by{" "} 
+                    <a href='/' target='_blank'>
+                        Dnocentini
+                    </a>.
+                    <br />
+                        Copyright © 2021.
+                </Typography>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
