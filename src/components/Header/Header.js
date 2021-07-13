3713c52852c559fc5ab226720717c9d04e74c261
Dinny Nocentini <dinny.nocentini@gmail.com>
2021-07-12 23:07:25 -0500

    style Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,15 +21,15 @@ const Header = (props) => {
     return (
         <Navbar expand='lg' sticky='top' className='header'>
             {/*Home link*/}
-            <Nav.Link as={NavLink} to='/'></Nav.Link>
-            <Navbar.Brand className='header_home'>
-                <HomeRounded />
-            </Navbar.Brand>
-
+            <Nav.Link as={NavLink} to='/' className='header_navlink'>
+                <Navbar.Brand className='header_home'>
+                    <HomeRounded />
+                </Navbar.Brand>
+            </Nav.Link>
             <Navbar.Toggle />
 
             <Navbar.Collapse>
-                <Nav>
+                <Nav className='header_left'>
                     {/* About link */}
                     <Nav.Link 
                         as={NavLink} 
