3abe733d0550516fdb0f9cf807f20b875769ea79
Dinny Nocentini <dinny.nocentini@gmail.com>
2021-07-13 13:17:49 -0500

    implement About Page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Grid, Typography } from '@material-ui/core';
+import React from 'react';
+import resumeData from '../../utils/resumeData';
+import './About.css';
+
 
 const About = () => {
     return (
-        <div>
-            About Page
-        </div>
-    )
-}
+        <>
+            {/* About me */}
+            <Grid container className='section pb_45'>
+                <Grid item className='section_title mb_30'>
+                    <span></span>
+                    <Typography variant='h6' className='section_title_text'>About Me</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant='body2' className='aboutme_text'>{resumeData.about}</Typography>
+                </Grid>
+            </Grid>
 
-export default About
+            {/* Resume */}
+            <Grid container className='section'></Grid>
+        </>
+    );
+};
+
+export default About;
